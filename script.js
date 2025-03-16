3f4d0053ce666645f55cb9950f1d18979dc2a0df
Moses-Onche <onchemoses38@gmail.com>
2025-03-16 13:17:51 +0100

    Write script to test haptic illusion when buttons are touched

---
+++ script.js
@@ -0,0 +1,5 @@
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+const down = document.getElementById("down");
+const rotate = document.getElementById("rotate");
+console.log(rotate);
