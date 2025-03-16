6df5ef432120081df2243ead20db5de0aeffdf8b
Moses-Onche <onchemoses38@gmail.com>
2025-03-16 15:29:53 +0100

    Use pointer for button touch event

--- script.js
+++ script.js
@@ -11,26 +11,26 @@ left.addEventListener("pointerup", () => {
     left.style.backgroundImage = 'url("./assets/left-0.png")';
 })
 
-right.addEventListener("mousedown", () => {
+right.addEventListener("pointerdown", () => {
     right.style.backgroundImage = 'url("./assets/right-1.png")';
 })
 
-right.addEventListener("mouseup", () => {
+right.addEventListener("pointerup", () => {
     right.style.backgroundImage = 'url("./assets/right-0.png")';
 })
 
-down.addEventListener("mousedown", () => {
+down.addEventListener("pointerdown", () => {
     down.style.backgroundImage = 'url("./assets/down-1.png")';
 })
 
-down.addEventListener("mouseup", () => {
+down.addEventListener("pointerup", () => {
     down.style.backgroundImage = 'url("./assets/down-0.png")';
 })
 
-rotate.addEventListener("mousedown", () => {
+rotate.addEventListener("pointerdown", () => {
     rotate.style.backgroundImage = 'url("./assets/rotate-1.png")';
 })
 
-rotate.addEventListener("mouseup", () => {
+rotate.addEventListener("pointerup", () => {
     rotate.style.backgroundImage = 'url("./assets/rotate-0.png")';
 })
