591f8d210589d452c8b85dc8b0243c5e16536c9b
Moses-Onche <onchemoses38@gmail.com>
2025-03-16 13:53:57 +0100

    Build haptic illusion using event listener

--- script.js
+++ script.js
@@ -2,4 +2,35 @@ const left = document.getElementById("left");
 const right = document.getElementById("right");
 const down = document.getElementById("down");
 const rotate = document.getElementById("rotate");
-console.log(rotate);
+
+left.addEventListener("mousedown", () => {
+    left.style.backgroundImage = 'url("./assets/left-1.png")';
+})
+
+left.addEventListener("mouseup", () => {
+    left.style.backgroundImage = 'url("./assets/left-0.png")';
+})
+
+right.addEventListener("mousedown", () => {
+    right.style.backgroundImage = 'url("./assets/right-1.png")';
+})
+
+right.addEventListener("mouseup", () => {
+    right.style.backgroundImage = 'url("./assets/right-0.png")';
+})
+
+down.addEventListener("mousedown", () => {
+    down.style.backgroundImage = 'url("./assets/down-1.png")';
+})
+
+down.addEventListener("mouseup", () => {
+    down.style.backgroundImage = 'url("./assets/down-0.png")';
+})
+
+rotate.addEventListener("mousedown", () => {
+    rotate.style.backgroundImage = 'url("./assets/rotate-1.png")';
+})
+
+rotate.addEventListener("mouseup", () => {
+    rotate.style.backgroundImage = 'url("./assets/rotate-0.png")';
+})
